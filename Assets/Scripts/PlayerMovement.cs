ebb8626a36910a9821284030f8ebd3aded781853
TtroubleTT <TtroubleTT2@gmail.com>
2025-01-11 22:47:39 -0800

    add project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,209 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform bodyTrans;
+
+    [Header("Speed")]
+    [SerializeField] private float walkSpeed = 12f;
+    [SerializeField] private float sprintSpeed = 20f;
+    [SerializeField] private float crouchSpeed = 5f;
+    private float _currentSpeed;
+
+    [Header("Physics")]
+    [SerializeField] private float gravity = - 9.81f;
+    private Vector3 velocity;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpHeight = 3f;
+
+    [Header("Crouching")]
+    [SerializeField] private float crouchYScale;
+    private float _startYScale;
+    private bool _isCrouching = false;
+    private double fallTime = 0.0;
+    private bool jumped = false;
+    
+    // For new input system
+    private Vector2 movementInput = Vector2.zero;
+    private bool _shouldSprint = false;
+    private bool _shouldCrouch = false;
+
+    // Movement States
+    private MovementState movementState;
+
+    public enum MovementState
+    {
+        Walking,
+        Sprinting,
+        Crouching,
+        Air,
+        Falling,
+    }
+    
+    // Code has been inspired and modified a bit based on these tutorials
+    // https://www.youtube.com/watch?v=f473C43s8nE&t=505s
+    // https://www.youtube.com/watch?v=_QajrabyTJc
+
+    public void UpdateSpeed(float change)
+    {
+        walkSpeed += change;
+        sprintSpeed += change;
+        crouchSpeed += change;
+    }
+
+    public float GetCurrentSpeed()
+    {
+        return _currentSpeed;
+    }
+
+    public MovementState GetCurrentMovementState()
+    {
+        return movementState;
+    }
+    
+    public bool IsGrounded()
+    {
+        return controller.isGrounded;
+    }
+    
+    private void Start()
+    {
+        _startYScale = transform.localScale.y;
+    }
+
+    private void Update()
+    {
+        // Handles what movement state we are in
+        MovementStateHandler();
+        
+        // Resets falling velocity if they are no longer falling
+        ResetVelocity();
+        
+        // Movement
+        MoveInDirection();
+
+        // Force standing if player isn't trying to crouch and is no longer under object
+        ForceStandUp();
+
+        // Gravity
+        Gravity();
+    }
+
+    private void MovementStateHandler()
+    {
+        // Determines the movement state and speed based on different conditions
+        if (_isCrouching)
+        {
+            movementState = MovementState.Crouching;
+            _currentSpeed = crouchSpeed;
+        }
+        else if (IsGrounded() && _shouldSprint)
+        {
+            movementState = MovementState.Sprinting;
+            _currentSpeed = sprintSpeed;
+        }
+        else if (IsGrounded())
+        {
+            movementState = MovementState.Walking;
+            _currentSpeed = walkSpeed;
+        }
+        else
+        {
+            if (fallTime < 0.2 && !jumped)
+            {
+                movementState = MovementState.Falling;
+                fallTime += Time.deltaTime;
+            }
+            else
+                movementState = MovementState.Air;
+        }
+    }
+
+    private void ResetVelocity()
+    {
+        if (IsGrounded() && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            jumped = false;
+            fallTime = 0.0;
+        }
+    }
+
+    private void MoveInDirection()
+    {
+        Transform myTransform = transform;
+        Vector3 move = myTransform.right * movementInput.x + myTransform.forward * movementInput.y; // This makes it so its moving locally so rotation is taken into consideration
+
+        controller.Move(move * (_currentSpeed * Time.deltaTime)); // Moving in the direction of move at the speed
+    }
+
+    private void DoJump() => velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    
+    private bool IsUnderObject()
+    {
+        float heightAbove = controller.height - crouchYScale; // height length between full stand and crouch
+        
+        // Ray casts upwards an amount to check if you are under and object. If the raycast hits nothing then you are above ground.
+        return Physics.Raycast(transform.position, Vector3.up, heightAbove);
+    }
+
+    private void ForceStandUp()
+    {
+        if (_isCrouching && !_shouldCrouch && !IsUnderObject())
+        {
+            Vector3 localScale = bodyTrans.localScale;
+            bodyTrans.localScale = new Vector3(localScale.x, _startYScale, localScale.z);
+            _isCrouching = false;
+        }
+    }
+
+    private void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    // New Input system actions below
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+        
+        if ((IsGrounded() || movementState == MovementState.Falling) && movementState != MovementState.Crouching)
+        {
+            jumped = true;
+            DoJump();
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        _shouldSprint = context.action.triggered;
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        Vector3 localScale = bodyTrans.localScale;
+        _shouldCrouch = context.action.triggered;
+        
+        if (context.started && !_isCrouching) // If we push down the crouch key and we are crouching we decrease model size
+        {
+            bodyTrans.localScale = new Vector3(localScale.x, crouchYScale, localScale.z);
+            _isCrouching = true;
+        }
+        else if (!context.performed && _isCrouching && !IsUnderObject()) // When releasing crouch key sets our scale back to normal
+        {
+            bodyTrans.localScale = new Vector3(localScale.x, _startYScale, localScale.z);
+            _isCrouching = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Telekinesis.cs
@@ -0,0 +1,255 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Telekinesis : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform camTrans;
+    [SerializeField] private PlayerMovement movement;
+    [SerializeField] private CharacterController controller;
+    
+    [Header("Raycast Info")]
+    [SerializeField] private float rayCastWidth = 3;
+    [SerializeField] private LayerMask telekinesisLayer;
+    
+    [Header("Grab Info")]
+    [SerializeField] private float grabSpeed = 10;
+    [SerializeField] private float maxDistanceFromPlayer = 20;
+    [SerializeField] private float minDistanceFromPlayer = 5;
+    [SerializeField] private float scrollChange = 1f;
+    private float distanceFromPlayer;
+    private bool grabbingObject = false;
+    private Transform grabbedObject;
+    private Rigidbody grabbedRb;
+    
+    [Header("Push Info")]
+    [SerializeField] private float defaultForce = 100f;
+    [SerializeField] private float maxForceMultiplier = 30;
+    [SerializeField] private float forceChange = 1f;
+    [SerializeField] private float tickTime = .03f;
+    private float currentMultiplier = 1;
+    private bool increaseMultiplier = false;
+    private float lastTick;
+
+    [Header("Rotate Info")] 
+    [SerializeField] private float rotationSpeed = 10f;
+    private bool rotate = false;
+
+    [Header("Raise Earth Info")] 
+    [SerializeField] private LayerMask earthMask;
+
+    public void OnPush(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            increaseMultiplier = true;
+            currentMultiplier = 1;
+            lastTick = Time.time;
+            return;
+        }
+        
+        if (!context.canceled)
+            return;
+        
+        increaseMultiplier = false;
+        
+        Vector3 camPos = camTrans.position;
+        Quaternion camRot = camTrans.rotation;
+        Vector3 forward = camTrans.forward;
+
+        if (grabbingObject)
+        {
+            PushGameObject(grabbedObject.gameObject, forward);
+            return;
+        }
+
+        RaycastHit[] hits = new RaycastHit[10];
+        int hitCount = Physics.BoxCastNonAlloc(camPos + (-forward * 2), new Vector3(rayCastWidth, rayCastWidth, rayCastWidth), forward, hits, camRot, maxDistanceFromPlayer, telekinesisLayer);
+
+        for (int i = 0; i < hitCount; i++)
+        {
+            PushGameObject(hits[i].transform.gameObject, forward);
+        }
+    }
+
+    public void OnGrab(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            grabbingObject = false;
+            Rigidbody rigid = grabbedObject.GetComponent<Rigidbody>();
+            if (rigid == null)
+                return;
+
+            rigid.useGravity = true;
+            return;
+        }
+        
+        if (!context.started)
+            return;
+        
+        Vector3 camPos = camTrans.position;
+        Quaternion camRot = camTrans.rotation;
+        Vector3 forward = camTrans.forward;
+
+        RaycastHit[] hits = new RaycastHit[10];
+        int hitCount = Physics.BoxCastNonAlloc(camPos + (-forward * 2), new Vector3(rayCastWidth, rayCastWidth, rayCastWidth), forward, hits, camRot, maxDistanceFromPlayer, telekinesisLayer);
+
+        if (hitCount <= 0)
+            return;
+
+        GameObject obj = hits[0].transform.gameObject;
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        grabbedObject = obj.transform;
+        grabbedRb = rb;
+        rb.useGravity = false;
+        grabbingObject = true;
+        distanceFromPlayer = Vector3.Distance(obj.transform.position, camTrans.position);
+    }
+
+    public void OnChangeDistance(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+        
+        Vector2 scrollInput = context.ReadValue<Vector2>();
+        float amountChange = scrollChange;
+        if (scrollInput.y < 0)
+        {
+            amountChange *= -1;
+        }
+
+        float newAmount = distanceFromPlayer + amountChange;
+        if (newAmount > maxDistanceFromPlayer)
+        {
+            distanceFromPlayer = maxDistanceFromPlayer;
+            return;
+        }
+
+        if (newAmount < minDistanceFromPlayer)
+        {
+            distanceFromPlayer = minDistanceFromPlayer;
+            return;
+        }
+
+        distanceFromPlayer += amountChange;
+    }
+
+    public void OnRotateObject(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            rotate = false;
+            return;
+        }
+        
+        if (!grabbingObject)
+            return;
+
+        if (context.started)
+        {
+            rotate = true;
+        }
+    }
+
+    public void OnRaiseEarth(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+        
+        Debug.Log("past context");
+        Vector3 camPos = camTrans.position;
+        Quaternion camRot = camTrans.rotation;
+        Vector3 forward = camTrans.forward;
+        
+        bool hit = Physics.Raycast(camPos, forward, out RaycastHit hitInfo, maxDistanceFromPlayer, earthMask);
+        if (!hit)
+            return;
+
+        Debug.Log("creating cube");
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = hitInfo.point;
+        cube.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    private void Start()
+    {
+        distanceFromPlayer = minDistanceFromPlayer;
+    }
+
+    private void Update()
+    {
+        UpdateGrabPosition();
+        UpdatePushIntensity();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateRotateObject();
+    }
+
+    private void UpdateGrabPosition()
+    {
+        if (!grabbingObject)
+            return;
+
+        float speed = grabSpeed;
+        if (controller.velocity.magnitude > 0)
+        {
+            speed += movement.GetCurrentSpeed();
+        }
+        
+        float step = speed * Time.deltaTime;
+        Vector3 targetPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceFromPlayer));
+        targetPos += Vector3.up * 0.9f;
+        grabbedObject.position = Vector3.MoveTowards(grabbedObject.position, targetPos, step);
+    }
+
+    private void UpdatePushIntensity()
+    {
+        if (!increaseMultiplier)
+            return;
+
+        if (Time.time - lastTick < tickTime)
+            return;
+
+        lastTick = Time.time;
+
+        float newAmount = currentMultiplier + forceChange;
+        if (newAmount > maxForceMultiplier)
+        {
+            currentMultiplier = maxForceMultiplier;
+            return;
+        }
+
+        currentMultiplier = newAmount;
+    }
+
+    private void UpdateRotateObject()
+    {
+        if (!rotate)
+            return;
+
+        if (!grabbingObject)
+            return;
+
+        grabbedRb.angularVelocity = Vector3.up * rotationSpeed;
+    }
+    
+    private void PushGameObject(GameObject obj, Vector3 direction)
+    {
+        grabbingObject = false;
+        
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        rb.useGravity = true;
+        rb.AddForce(direction * (defaultForce * currentMultiplier), ForceMode.Force);
+    }
+}
